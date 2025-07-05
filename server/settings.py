8eb7246b143099c736e0825f9a61219cf5f20736
Tumati-Sasirekha <161676646+Tumati-Sasirekha@users.noreply.github.com>
2025-07-05 15:04:35 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k=j8ok2nhuw2%a9-nr&oa44vycfdm#m$fr&n@hgn*9!=--hgsx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
